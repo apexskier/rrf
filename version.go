2e5e0a5facb6a5445e7a5a02e8e55f64e63fc1c2
Cameron Little <cameron@camlittle.com>
2020-12-20 14:07:00 +0100

    Add version info and automatic release workflows (#2)

---
+++ version.go
@@ -0,0 +1,7 @@
+package main
+
+// GitCommit is the git commit at time of release build
+var GitCommit string = "HEAD"
+
+// Version is public facing version at time of release build
+var Version string = "dev"
